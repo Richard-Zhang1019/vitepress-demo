8e0252c5bf2e56af0b3ceb659e45c1991e82b253
Richard Zhang <1501109542@qq.com>
2023-01-26 19:12:31 +0800

    chore: copy

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vitepress'
-import { NavItem, Sidebar, SocialLink } from '../../types/vitepress'
+import type { NavItem, Sidebar, SocialLink } from '../../types/vitepress'
 
 export default defineConfig({
   title: 'lucky-design',
   description: 'Just playing around',
-  lang: 'cn-ZH',
+  lang: 'zh-CN',
+  appearance: 'dark',
   base: '/',
+  head: [
+    ['link', { rel: 'icon', href: '/public/icon.ico' }],
+  ],
   themeConfig: {
+    logo: '/public/icon.png',
     // 网站header部分标题
     siteTitle: 'lucky-design',
     // 顶部tab-bar
@@ -19,33 +24,47 @@ export default defineConfig({
     sidebar: [
       {
         text: 'Guide',
+        collapsible: true,
         items: [
           { text: 'about', link: '/about' },
-          { text: 'profile', link: '/profile' }
-        ]
-      }
+          { text: 'profile', link: '/profile' },
+        ],
+      },
+      {
+        text: 'Components',
+        collapsible: true,
+        items: [
+          { text: 'button', link: '/button' },
+          { text: 'icon', link: '/icon' },
+        ],
+      },
     ] as Sidebar,
-    outlineTitle: 'In hac pagina',
+    outlineTitle: 'In hac page',
     // 社交账号链接
     socialLinks: [
       {
         icon: 'github',
-        link: 'https://github.com/ByteDanceYouthTrainCamp/lucky-design'
-      }
+        link: 'https://github.com/ByteDanceYouthTrainCamp/lucky-design',
+      },
     ] as SocialLink[],
     // footer
     footer: {
       message: 'Released under the MIT License.',
-      copyright: 'Copyright © 2019-present Evan You'
+      copyright: 'Copyright © 2023-present Lucky-Design',
     },
     editLink: {
       pattern: 'https://github.com/vuejs/vitepress/edit/main/docs/:path',
-      text: 'Edit this page on GitHub'
-    }
+      text: 'Edit this page on GitHub',
+    },
+    algolia: {
+      appId: 'Z7DZ7S5F34',
+      apiKey: 'f2fc637edfcc09751dcea9a8e26350f4',
+      indexName: 'lucky-design',
+    },
   },
   lastUpdated: true,
   markdown: {
     theme: 'material-darker',
-    lineNumbers: true
-  }
+    lineNumbers: true,
+  },
 })
--- types/vitepress.ts
+++ types/vitepress.ts
@@ -1,4 +1,4 @@
-export type NavItemWithLink = {
+export interface NavItemWithLink {
   text: string
   link: string
   activeMatch?: string
